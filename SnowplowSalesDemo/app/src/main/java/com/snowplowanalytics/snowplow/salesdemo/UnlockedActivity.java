102815880c854aaa7ca3eaeae6e91f938826888b
Matúš Tomlein <matus.tomlein@gmail.com>
2021-10-22 14:58:06 +0200

    Send self-describing event from Unlocked page

--- SnowplowSalesDemo/app/src/main/java/com/snowplowanalytics/snowplow/salesdemo/UnlockedActivity.java
+++ SnowplowSalesDemo/app/src/main/java/com/snowplowanalytics/snowplow/salesdemo/UnlockedActivity.java
@@ -3,11 +3,30 @@ package com.snowplowanalytics.snowplow.salesdemo;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.snowplowanalytics.snowplow.Snowplow;
+import com.snowplowanalytics.snowplow.controller.TrackerController;
+import com.snowplowanalytics.snowplow.event.SelfDescribing;
+import com.snowplowanalytics.snowplow.payload.SelfDescribingJson;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class UnlockedActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unlocked);
+
+        TrackerController tracker = Snowplow.getDefaultTracker();
+
+        Map<String, String> properties = new TreeMap<>();
+        properties.put("greeting", "Hello from Android!");
+        properties.put("message", "Snowplow data is always in the same format, you won't need different data models for web and mobile");
+        SelfDescribingJson sdj = new SelfDescribingJson("iglu:com.snowplowanalytics.snowplow/web_mobile_demo/jsonschema/1-0-0", properties);
+
+        SelfDescribing event = new SelfDescribing(sdj);
+
+        tracker.track(event);
     }
 }
